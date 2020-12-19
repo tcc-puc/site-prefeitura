0829962ade12a07f3e47c365f35491809add170c
Luan Fagundes <luan.fagundes@pebmed.com.br>
2020-12-19 18:44:03 -0300

    configuracao sass

---
+++ components/footer/index.jsx
@@ -0,0 +1,13 @@
+import "./style.scss"
+
+export default function Footer() {
+    return (
+      <>
+        <footer>
+            <p>Rua Alfredo Chaves, 1333
+            Caxias do Sul, RS — CEP 95020-460</p>
+            <p>(54) 3218-6000 — contato@caxias.rs.gov.br</p>
+        </footer>
+      </>
+    )
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+
 import Header from '../components/header'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
@@ -51,16 +53,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <Footer />
 
       <style jsx>{`
         .container {
