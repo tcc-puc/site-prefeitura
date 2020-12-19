5615e08aa4536c5ec7dddef86a73a0194029f5f9
Luan Fagundes <luan.fagundes@pebmed.com.br>
2020-12-19 18:59:42 -0300

    footer do site

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -4,9 +4,18 @@ export default function Footer() {
     return (
       <>
         <footer>
-            <p>Rua Alfredo Chaves, 1333
-            Caxias do Sul, RS — CEP 95020-460</p>
-            <p>(54) 3218-6000 — contato@caxias.rs.gov.br</p>
+          <div className="wrapper">
+            <div className="content">
+              <p>Rua Alfredo Chaves, 987 <br />
+              Bom Destino, MG — CEP 95020-460</p>
+              <p>(54) 3218-6000 — <a href="javascript:void(0)">contato@bomdestino.gov.br</a></p>
+            </div>
+            <div className="content">
+            <p>© 2020 Prefeitura de Bom Destino <br />
+              <a href="javascript:void(0)">Sobre o portal</a>
+            </p>
+            </div>
+          </div>
         </footer>
       </>
     )
