b51a802dfa6464d0b1c28b58170e190d8bc4affc
Karol Harumi <karol.harumi@hotmail.com>
2020-12-29 22:53:46 -0300

    Correcao de id e name dos campos de formulario do perfil de paciente

--- components/patientProfile/index.jsx
+++ components/patientProfile/index.jsx
@@ -10,8 +10,8 @@ export default function PatientProfile() {
         <FontAwesomeIcon icon={faUser} color="#ef8354" className="icon-user" />
 
         <div className="field-container">
-          <label for="fname">Número de inscrição</label>
-          <input type="text" id="fname" name="fname" value="000123" disabled="disabled" />
+          <label for="fid">Número de inscrição</label>
+          <input type="text" id="fid" name="fid" value="000123" disabled="disabled" />
         </div>
 
         <div className="field-container">
@@ -20,58 +20,58 @@ export default function PatientProfile() {
         </div>
 
         <div className="field-container">
-          <label for="fname">Idade</label>
-          <input type="text" id="fname" name="fname" value="29 anos (19/10/1991)" disabled="disabled" />
+          <label for="fage">Idade</label>
+          <input type="text" id="fage" name="fage" value="29 anos (19/10/1991)" disabled="disabled" />
         </div>
 
         <div className="field-container">
-          <label for="fname">Celular</label>
-          <input type="text" id="fname" name="fname" value="(11) 96966-0123" />
+          <label for="fphone">Celular</label>
+          <input type="text" id="fphone" name="fphone" value="(11) 96966-0123" />
         </div>
 
         <div className="field-container">
-          <label for="fname">CEP</label>
-          <input type="text" id="fname" name="fname" value="08230-030" disabled="disabled" />
+          <label for="fzipcode">CEP</label>
+          <input type="text" id="fzipcode" name="fzipcode" value="08230-030" disabled="disabled" />
         </div>
 
         <div className="field-container">
-          <label for="fname">Endereço</label>
-          <input type="text" id="fname" name="fname" value="Jardim Fagundes" disabled="disabled" />
+          <label for="faddress">Endereço</label>
+          <input type="text" id="faddress" name="faddress" value="Jardim Fagundes" disabled="disabled" />
         </div>
 
         <div className="field-container">
-          <label for="fname">Número</label>
-          <input type="text" id="fname" name="fname" value="123" disabled="disabled" />
+          <label for="fnumber">Número</label>
+          <input type="text" id="fnumber" name="fnumber" value="123" disabled="disabled" />
         </div>
 
         <div className="field-container">
-          <label for="fname">Complemento</label>
-          <input type="text" id="fname" name="fname" value="Bloco 5 Apto 122" disabled="disabled" />
+          <label for="fcomplement">Complemento</label>
+          <input type="text" id="fcomplement" name="fcomplement" value="Bloco 5 Apto 122" disabled="disabled" />
         </div>
 
         <div className="field-container">
-          <label for="fname">CPF</label>
-          <input type="text" id="fname" name="fname" value="000.000.000-00" disabled="disabled" />
+          <label for="fpersonalid">CPF</label>
+          <input type="text" id="fpersonalid" name="fpersonalid" value="000.000.000-00" disabled="disabled" />
         </div>
 
         <div className="field-container">
-          <label for="fname">Sexo</label>
-          <input type="text" id="fname" name="fname" value="F" disabled="disabled" />
+          <label for="fgender">Sexo</label>
+          <input type="text" id="fgender" name="fgender" value="F" disabled="disabled" />
         </div>
 
         <div className="field-container">
-          <label for="fname">Observação</label>
-          <input type="text" id="fname" name="fname" value="-" />
+          <label for="fobservation">Observação</label>
+          <input type="text" id="fobservation" name="fobservation" value="-" />
         </div>
 
         <div className="field-container">
-          <label for="fname">Alergias</label>
-          <textarea type="text" id="fname" name="fname" value="Rinite" />
+          <label for="fallergies">Alergias</label>
+          <textarea type="text" id="fallergies" name="fallergies" value="Rinite" />
         </div>
 
         <div className="field-container">
-          <label for="fname">Medicamentos</label>
-          <textarea type="text" id="fname" name="fname" value="Medicamento 1 e Medicamento 2" />
+          <label for="fmedicines">Medicamentos</label>
+          <textarea type="text" id="fmedicines" name="fmedicines" value="Medicamento 1 e Medicamento 2" />
         </div>
       </fieldset>
     )
